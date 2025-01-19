15d6402ebf9de2dd608bdefece27404e3e6f51e5
LauraMFerrisNY <LauraMFerrisNY@gmail.com>
2025-01-19 16:40:46 -0500

    Completed Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import { useState } from 'react'
-import ContactList from './components/ContactList'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import ContactList from "./components/ContactList";
+import SelectedContact from "./components/SelectedContact";
 
-function App() {
-  const [selectedContactId, setSelectedContactId] = useState(null)
+
+export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
   
   return (
     <>
       {selectedContactId ? (
-        <div>Selected Contact View</div>
+        <SelectedContact selectedContactId={selectedContactId} setSelectedContactId={setSelectedContactId} />
       ) : (
-        <ContactList setSelectedContactId={setSelectedContactId} />
-        
+        <ContactList setSelectedContactId={setSelectedContactId}/>
       )}
     </>
   );
 }
-
-export default App
